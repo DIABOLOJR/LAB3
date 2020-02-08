129c45750f1e5b37aae674e19ccb2162fb6d68b6
Estuardo Mancio <man18027@uvg.edu.gt>
2020-02-08 11:59:25 -0600

    Comienzo Del programa
    Aqui se realizara el laboratorio 3

---
+++ LCDMAIGA.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=LCDMAIGA.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/LCDMAIGA.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=lcdmaiga.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/lcdmaiga.x.tar
